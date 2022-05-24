53b8d3cc7d2434e00448fafbd9e11eff2a595819
grittaweisheit <grittaweisheit@gmail.com>
2022-05-24 12:01:37 +0200

    fix missing imoports

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v1/corporate.proto
+protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v1/utils.proto proto/bakdata/corporate/v1/announcement.proto proto/bakdata/corporate/v1/person.proto proto/bakdata/corporate/v1/institution.proto
