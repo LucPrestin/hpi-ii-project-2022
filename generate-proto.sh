2c68df684ac31211c0ec65b39146ba51082cd140
LucPrestin <LucPrestin@users.noreply.github.com>
2022-06-23 19:44:59 +0200

    add fixing paths in protoc generated fields

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v1/utils.proto proto/bakdata/corporate/v1/announcement.proto proto/bakdata/corporate/v1/person.proto proto/bakdata/corporate/v1/institution.proto
+
+for filename in ./build/gen/bakdata/corporate/v1/*.py; do
+    [ -e "$filename" ] || continue
+
+    sed -i 's/from bakdata/from build.gen.bakdata/gI' $filename
+done
